f4a7f2b9e5040ebe238e0f9c00845b88a7d7e01a
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-18 20:34:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names=['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(names,event){
+    const messages=[]
+    for(let i=0;i<names.length;i++){
+     messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(names));
+function countDown(number){
+    while(number>=0){
+        console.log(number--);
+    }
+}
+countDown(10);
